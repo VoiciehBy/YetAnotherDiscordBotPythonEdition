b03f441dd67af2c248edf1e4c61462b22a36ec6a
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-03-29 05:26:11 +0200

    Small bugfix with displaying song names, that have '.' (comma) characters...

--- src/commands.py
+++ src/commands.py
@@ -30,7 +30,7 @@ async def playSong(voiceClient, song_name_with_ext):
             voiceClient.play(discord.FFmpegPCMAudio(source=song_path))
             
     start = song_path.find("\music") + 7
-    end = song_path.find(".")
+    end = song_path.find("." + constants.default_music_download_extension)
     constants.current_song_name = song_path[start:end]
 
 
