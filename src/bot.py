f674bee725811d7c3bd24cb52954175f7907eca8
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-09-20 20:55:47 +0200

    Refactoring...

--- src/bot.py
+++ src/bot.py
@@ -1,45 +1,36 @@
 import discord
 import commands
 import utils as u
-import constants
+import constants as c
 
 intents = discord.Intents.default()
 intents.message_content = True
-botClient = discord.Client(intents=intents)
-
-
-@botClient.event
-async def on_ready():
-    print(f"{botClient.user} has connected!11")
-    if(constants.autojoin):
-        voiceChannel = u.getVoiceChannel(botClient)
-        await commands.joinVoiceChannel(botClient, voiceChannel)
-
+botClient = commands.botClient(intents=intents)
 
 @botClient.event
 async def handleCommands(msg):
     if (u.isCommmand(msg)):
         cmd_name = u.getCommandName(msg)
         cmd_arg = u.getCommandArguments(msg)
-        if (cmd_name == constants.commands[0]):
+        if (cmd_name == c.commands[0]):
             await commands.sayHi(msg.channel)
-        elif (cmd_name == constants.commands[1]):
+        elif (cmd_name == c.commands[1]):
             voiceChannel = u.getVoiceChannel(botClient)
-            await commands.joinVoiceChannel(botClient, voiceChannel)
-        elif (cmd_name == constants.commands[2]):
-            await msg.channel.send(constants.byReply)
-            await commands.disconnect(botClient)
-        elif (cmd_name == constants.commands[3]):
-            await msg.channel.send(constants.next_txt + u.song_title(u.music_url(cmd_arg)))
+            await botClient.joinVoiceChannel(voiceChannel)
+        elif (cmd_name == c.commands[2]):
+            await msg.channel.send(c.byReply)
+            await botClient.disconnect()
+        elif (cmd_name == c.commands[3]):
+            await msg.channel.send(c.next_txt + u.song_title(u.music_url(cmd_arg)))
             if(await commands.playSong(botClient.voice_clients[0], cmd_arg) == 1):
-                await msg.channel.send(constants.now_txt + u.song_title(u.music_url(cmd_arg)))
+                await msg.channel.send(c.now_txt + u.song_title(u.music_url(cmd_arg)))
             elif(await commands.playSong(botClient.voice_clients[0], cmd_arg) == -1):
-                await msg.channel.send(constants.playa_busy_txt)
-        elif (cmd_name == constants.commands[4]):
+                await msg.channel.send(c.playa_busy_txt)
+        elif (cmd_name == c.commands[4]):
             await commands.stopSong(botClient.voice_clients[0])
-        elif (cmd_name == constants.commands[5]):
+        elif (cmd_name == c.commands[5]):
             await commands.purgeChannel(msg.channel)
-        elif (cmd_name == constants.commands[6]):
+        elif (cmd_name == c.commands[6]):
             await commands.printHelp(msg.channel)
         else:
             await commands.sayThereIsNoSuchCommand(msg.channel)
@@ -47,9 +38,9 @@ async def handleCommands(msg):
 
 @botClient.event
 async def on_message(msg):
-    if(not(u.ifMsgOnTheChannel(msg, constants.BOT_TXT_CHANNEL_ID)) or u.ifMsgComesFromBot(msg)):
+    if(not(u.ifMsgOnTheChannel(msg, c.BOT_TXT_CHANNEL_ID)) or u.ifMsgComesFromBot(msg)):
         return
     else:
         await handleCommands(msg)
 
-botClient.run(constants.BOT_TOKEN)
+botClient.run(c.BOT_TOKEN)
--- src/commands.py
+++ src/commands.py
@@ -1,47 +1,58 @@
-import constants
-import utils
-import discord
+import discord as d
+import constants as c
+import utils as u
+
+
+class botClient(d.Client):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def joinVoiceChannel(self, channel):
+        voiceChannel = self.get_channel(channel.id)
+        print(c.hiReply)
+        if (not (voiceChannel)):
+            return
+        elif (not (self.voice_clients)):
+            voiceClient = await voiceChannel.connect()
+
+    async def on_ready(self):
+        print(f"{botClient.user} has connected!11")
+        if (c.autojoin):
+            voiceChannel = u.getVoiceChannel(self)
+            await self.joinVoiceChannel(voiceChannel)
+
+    async def disconnect(self):
+        print(c.byReply)
+        if (self.voice_clients):
+            await self.voice_clients[0].disconnect()
+        await botClient.close()
 
 
 async def sayHi(txtChannel):
-    await txtChannel.send(constants.hiReply)
-
-
-async def joinVoiceChannel(botClient, channel):
-    voiceChannel = botClient.get_channel(channel.id)
-    print(constants.hiReply)
-    if(not(voiceChannel)):
-        return
-    elif(not(botClient.voice_clients)):
-        voiceClient = await voiceChannel.connect()
-
-
-async def disconnect(botClient):
-    print(constants.byReply)
-    if(botClient.voice_clients):
-        await botClient.voice_clients[0].disconnect()
-    await botClient.close()
+    await txtChannel.send(c.hiReply)
 
 
 async def playSong(voiceClient, song_name):
-    song_path = utils.download_music(utils.music_url(song_name)).replace('|', '_')#fix
+    song_path = u.download_music(u.music_url(song_name)).replace(
+        '|', '_').replace(',', '_')  # fix
 
-    if(voiceClient):
-        if(not(voiceClient.is_playing()) and (voiceClient)):
-            voiceClient.play(discord.FFmpegPCMAudio(source=song_path))
+    if (voiceClient):
+        if (not (voiceClient.is_playing()) and (voiceClient)):
+            voiceClient.play(d.FFmpegPCMAudio(source=song_path))
             return 1
         else:
-            print(constants.playa_busy_txt)
+            print(c.playa_busy_txt)
             return -1
 
 
 async def stopSong(voiceClient):
-    if(voiceClient):
+    if (voiceClient):
         voiceClient.stop()
 
 
 async def printHelp(txtChannel):
-    await txtChannel.send(utils.commandsHelp())
+    await txtChannel.send(u.commandsHelp())
 
 
 async def purgeChannel(ch):
@@ -49,4 +60,4 @@ async def purgeChannel(ch):
 
 
 async def sayThereIsNoSuchCommand(txtChannel):
-    await txtChannel.send(constants.defaultReply)
+    await txtChannel.send(c.defaultReply)
