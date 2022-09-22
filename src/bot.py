57989810ee90035d2f5062ae01f477ae12a385f8
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-09-22 16:28:34 +0200

    Refactoring...

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,11 @@
 import discord
-import commands
+import botClient as bC
 import utils as u
 import constants as c
 
 intents = discord.Intents.default()
 intents.message_content = True
-botClient = commands.botClient(intents=intents)
+botClient = bC.botClient(intents=intents)
 
 
 botClient.run(c.BOT_TOKEN)
