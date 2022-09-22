fe481f315e801e13c76dd636073069ab71af35ce
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-09-22 16:26:30 +0200

    Refactoring...

--- src/bot.py
+++ src/bot.py
@@ -7,40 +7,5 @@ intents = discord.Intents.default()
 intents.message_content = True
 botClient = commands.botClient(intents=intents)
 
-@botClient.event
-async def handleCommands(msg):
-    if (u.isCommmand(msg)):
-        cmd_name = u.getCommandName(msg)
-        cmd_arg = u.getCommandArguments(msg)
-        if (cmd_name == c.commands[0]):
-            await commands.sayHi(msg.channel)
-        elif (cmd_name == c.commands[1]):
-            voiceChannel = u.getVoiceChannel(botClient)
-            await botClient.joinVoiceChannel(voiceChannel)
-        elif (cmd_name == c.commands[2]):
-            await msg.channel.send(c.byReply)
-            await botClient.disconnect()
-        elif (cmd_name == c.commands[3]):
-            await msg.channel.send(c.next_txt + u.song_title(u.music_url(cmd_arg)))
-            if(await commands.playSong(botClient.voice_clients[0], cmd_arg) == 1):
-                await msg.channel.send(c.now_txt + u.song_title(u.music_url(cmd_arg)))
-            elif(await commands.playSong(botClient.voice_clients[0], cmd_arg) == -1):
-                await msg.channel.send(c.playa_busy_txt)
-        elif (cmd_name == c.commands[4]):
-            await commands.stopSong(botClient.voice_clients[0])
-        elif (cmd_name == c.commands[5]):
-            await commands.purgeChannel(msg.channel)
-        elif (cmd_name == c.commands[6]):
-            await commands.printHelp(msg.channel)
-        else:
-            await commands.sayThereIsNoSuchCommand(msg.channel)
-
-
-@botClient.event
-async def on_message(msg):
-    if(not(u.ifMsgOnTheChannel(msg, c.BOT_TXT_CHANNEL_ID)) or u.ifMsgComesFromBot(msg)):
-        return
-    else:
-        await handleCommands(msg)
 
 botClient.run(c.BOT_TOKEN)
--- src/commands.py
+++ src/commands.py
@@ -16,48 +16,82 @@ class botClient(d.Client):
         elif (not (self.voice_clients)):
             voiceClient = await voiceChannel.connect()
 
+    async def disconnect(self):
+        print(c.byReply)
+        if (self.voice_clients):
+            await self.voice_clients[0].disconnect()
+        await self.close()
+
+    async def playSong(self, voiceClient, song_name):
+        song_path = u.download_music(u.music_url(song_name)).replace(
+            '|', '_').replace(',', '_')  # fix
+
+        if (voiceClient):
+            if (not (voiceClient.is_playing())):
+                voiceClient.play(d.FFmpegPCMAudio(source=song_path))
+                return 1
+            else:
+                print(c.playa_busy_txt)
+                return -1
+
+    async def stopSong(self, voiceClient):
+        if (voiceClient):
+            voiceClient.stop()
+
+    async def sendMessage(self, txtChannel, txt):
+        await txtChannel.send(txt)
+
+    async def sayHi(self, txtChannel):
+        await self.sendMessage(txtChannel, c.hiReply)
+
+    async def sayBye(self, txtChannel):
+        await self.sendMessage(txtChannel, c.byReply)
+
+    async def purgeChannel(self, ch):
+        await ch.purge()
+
+    async def printHelp(self, txtChannel):
+        await txtChannel.send(u.commandsHelp())
+
+    async def sayThereIsNoSuchCommand(self, txtChannel):
+        await self.sendMessage(txtChannel, c.defaultReply)
+
     async def on_ready(self):
         print(f"{self.user} has connected!11")
         if (c.autojoin):
             voiceChannel = u.getVoiceChannel(self)
             await self.joinVoiceChannel(voiceChannel)
 
-    async def disconnect(self):
-        print(c.byReply)
-        if (self.voice_clients):
-            await self.voice_clients[0].disconnect()
-        await botClient.close()
+    async def handleCommands(self, msg):
+        if (u.isCommmand(msg)):
+            cmd_name = u.getCommandName(msg)
+            cmd_arg = u.getCommandArguments(msg)
+            if (cmd_name == c.commands[0]):
+                await self.sayHi(msg.channel)
+            elif (cmd_name == c.commands[1]):
+                voiceChannel = u.getVoiceChannel(self)
+                await self.joinVoiceChannel(voiceChannel)
+            elif (cmd_name == c.commands[2]):
+                await self.sayBye(msg.channel)
+                await self.disconnect()
+            elif (cmd_name == c.commands[3]):
+                song_title = u.song_title(u.music_url(cmd_arg))
+                await self.sendMessage(msg.channel, c.next_txt + song_title)
+                if (await self.playSong(self.voice_clients[0], cmd_arg) == 1):
+                    await self.sendMessage(msg.channel, c.now_txt + song_title)
+                elif (await self.playSong(self.voice_clients[0], cmd_arg) == -1):
+                    await self.sendMessage(msg.channel, c.playa_busy_txt)
+            elif (cmd_name == c.commands[4]):
+                await self.stopSong(self.voice_clients[0])
+            elif (cmd_name == c.commands[5]):
+                await self.purgeChannel(msg.channel)
+            elif (cmd_name == c.commands[6]):
+                await self.printHelp(msg.channel)
+            else:
+                await self.sayThereIsNoSuchCommand(msg.channel)
 
-
-async def sayHi(txtChannel):
-    await txtChannel.send(c.hiReply)
-
-
-async def playSong(voiceClient, song_name):
-    song_path = u.download_music(u.music_url(song_name)).replace(
-        '|', '_').replace(',', '_')  # fix
-
-    if (voiceClient):
-        if (not (voiceClient.is_playing()) and (voiceClient)):
-            voiceClient.play(d.FFmpegPCMAudio(source=song_path))
-            return 1
+    async def on_message(self, msg):
+        if (not (u.ifMsgOnTheChannel(msg, c.BOT_TXT_CHANNEL_ID)) or u.ifMsgComesFromBot(msg)):
+            return
         else:
-            print(c.playa_busy_txt)
-            return -1
-
-
-async def stopSong(voiceClient):
-    if (voiceClient):
-        voiceClient.stop()
-
-
-async def printHelp(txtChannel):
-    await txtChannel.send(u.commandsHelp())
-
-
-async def purgeChannel(ch):
-    await ch.purge()
-
-
-async def sayThereIsNoSuchCommand(txtChannel):
-    await txtChannel.send(c.defaultReply)
+            await self.handleCommands(msg)
